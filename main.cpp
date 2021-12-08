5038a4bf89a38afbfcc71b3eea8a1389cf990341
Liam Turnbull <ghostboy21134@gmail.com>
2021-12-08 20:33:31 +0000

    first version of the recursion part of the assignment has a seg fault issue though

--- main.cpp
+++ main.cpp
@@ -3,11 +3,32 @@
 #include <iostream>
 using namespace std;
 
+int factorialR(int);
 
 int main() 
 {
+  int intput;
+  cout << "enter the input for a factorial:";
+  cin >> intput;
 
-
+  cout <<endl << factorialR(intput);
 
 
 } 
+
+int factorialR(int i)
+{
+  // this function should take the starting number and multpily it down for example if the input is 7 then do 7 * 6 * 5 * 4 * 3 * 2 * 1. this would then return the result after all of have instanced
+
+  if(i > 0)// checks to see if i is 
+  {
+    int temp;
+    temp = i - 1;
+    i = i * temp;
+  
+    
+
+    return factorialR(i-1 );
+  }
+  else return i;
+}
