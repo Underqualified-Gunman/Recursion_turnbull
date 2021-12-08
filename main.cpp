68b93694be214793ad26145320a810428b1a7c4b
Liam Turnbull <ghostboy21134@gmail.com>
2021-12-08 20:42:06 +0000

    simplified the factorialR function but have the issue of only returning the variable inputted.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 //created by liam turnbull for dr T at TCC
-
+#include "IVE.h"
 #include <iostream>
 using namespace std;
 
@@ -20,15 +20,16 @@ int factorialR(int i)
 {
   // this function should take the starting number and multpily it down for example if the input is 7 then do 7 * 6 * 5 * 4 * 3 * 2 * 1. this would then return the result after all of have instanced
 
-  if(i > 0)// checks to see if i is 
+  if(i < 0)// checks to see if i is greater than 0
   {
-    int temp;
-    temp = i - 1;
-    i = i * temp;
-  
+    //int temp;
+    //temp = i - 1;
+    //i = i * temp;
+    // simplified the above code by multiplying i in the return line.
     
 
-    return factorialR(i-1 );
+    return (i * factorialR(i-1));
   }
+  // below 
   else return i;
 }
