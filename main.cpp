c653bc93198fa990fd1b2602cf1be4e81f6eef67
Liam Turnbull <ghostboy21134@gmail.com>
2021-12-08 23:42:57 +0000

    inital implementation of the honoi disc game problem solver has an issue where peg 1 gets moved to 3 2 times despite that being illeagal

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int factorialR(int);
+void hanoi(int, int, int, int);
 
 int main() 
 {
@@ -13,8 +14,19 @@ int main()
 
   //cout << factorialR(6) << endl;
   cout << factorialR(intput) << endl;
-  //cout << factorialR(5) << endl;
+  cout << "End of Factorials start of Towers of Hanoi" << endl;
+  //
+  cout << "start of tower of hanoi solution" << endl;
+  const int NUM_DISCS = 3;
+  const int LEFT_PEG = 1;
+  const int RIGHT_PEG = 3;
+  const int MIDDLE_PEG = 2;
 
+  hanoi( NUM_DISCS, LEFT_PEG, MIDDLE_PEG, RIGHT_PEG);
+
+
+  cout << "tower of hanoi is solved" << endl;
+  return 0;
 } 
 
 int factorialR(int i)
@@ -29,4 +41,28 @@ int factorialR(int i)
   {
     return i * factorialR(i-1);
   }
+}
+
+void hanoi(int i, int peg1, int peg2, int peg3)
+{
+  // peg 1 should be the peg that all the discs start on. 
+  // peg 2 is the temporary peg that allows you to slowly move discs around
+  // peg 3 is the goal peg and you want to have all the discs on this peg without having any larger onces on top
+  // i holds the number of discs that need to be moved 
+  // calling this funtion once will make it attempt to solve
+  // the tower of hanoi problem if it is unable to a if else block will break the solution down some this is then iterated upon until its solved
+  // rules
+  //Only one disc may be moved at a time.
+  //A disc cannot be placed on top of a smaller disc.
+  //All discs must be stored on a peg except while being moved.
+
+  if (i > 0)
+  {
+    hanoi(i - 1, peg1, peg2, peg3);
+    cout << "moving a disc from peg " << peg1 << "to peg" << peg3 << endl;
+    hanoi(i - 1, peg2, peg3, peg1);
+
+
+  }
+
 }
